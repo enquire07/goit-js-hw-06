4270fa51aaa27a5a2c36103dfc33e5ec48335989
enquire07 <145021641+enquire07@users.noreply.github.com>
2024-03-27 21:37:34 +0800

    js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", function (event) {
+  event.preventDefault(); // Prevent page reload
+
+  const emailInput = this.elements.email;
+  const passwordInput = this.elements.password;
+
+  if (emailInput.value === "" || passwordInput.value === "") {
+    alert("All fields must be filled in.");
+  } else {
+    const formData = {
+      email: emailInput.value,
+      password: passwordInput.value,
+    };
+
+    console.log(formData);
+
+    this.reset(); // Clear form fields
+  }
+});
--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+//
+
+const colorSpan = document.querySelector(".color");
+const changeColorBtn = document.querySelector(".change-color");
+
+changeColorBtn.addEventListener("click", function () {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  colorSpan.textContent = randomColor;
+});
